2d5890b17c1501aa56512f64b487ba2335cb6319
Angel Aguero <angelagueror23@gmail.com>
2024-01-24 23:48:04 -0400

    Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,16 @@
+<script setup lang="ts">
+import  NavBar from './views/NavBar.vue';
+
+</script>
+
 <template>
-  <div class="container">
-    <h1 class="text-size-8  ui header text-primary">Hola Mundo</h1>
-    <i class="american sign language interpreting icon"></i>
+  <NavBar/>
+  <div class="ui container" >
+    <RouterView />
+    <h1>Hola Mundo!</h1>
   </div>
 </template>
 
 <style scoped>
-.container {
-  width: 600px;
-  height: auto;
-  margin: 0 auto;
-}
+
 </style>
---
+++ client/src/views/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav class="nav waikawa-bg-800">
+    <input type="checkbox" name="nav-check" id="nav-check" />
+    <header class="nav-header">
+      <div class="nav-title text-size-6 waikawa-200"><i class="trademark icon"></i></div>
+    </header>
+    <div class="nav-links ">
+      <a class="text-size-3 waikawa-200 " href="#">Perfil</a>
+      <a class="text-size-3 waikawa-200" href="//github.io/jo_geek" target="_blank">Github</a>
+      <a class="text-size-3 waikawa-200" href="http://stackoverflow.com/users/4084003/" target="_blank">Stackoverflow</a>
+      
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+*{
+  box-sizing: border-box;
+}
+.nav {
+  height: 50px;
+  width: 100%;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  margin: 0px;
+  font-family: 'segoe ui';
+}
+
+.nav > nav-header {
+  display: inline;
+}
+.nav-title {
+  margin: 9px;
+  text-align: right;
+
+  
+}
+
+.nav > .nav-btn{
+  display: none;
+}
+.nav > .nav-links{
+  display: inline;
+  justify-content: end;
+  
+}
+
+.nav > .nav-links > a {
+  display: inline-block;
+  padding: 13px;
+  margin: 0 5px;
+  text-decoration: none;
+  
+
+}
+.nav > .nav-links > a:hover {
+  background-color: rgba(0, 0, 0, 0.3);
+  outline: 4px solid rgba(0, 0, 0, 0.3);
+}
+.nav > #nav-check {
+  display: none;
+}
+
+</style>
