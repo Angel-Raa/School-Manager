41b280293af4d69a30b6d5e5f3a20616871ab217
Angel Aguero <angelagueror23@gmail.com>
2024-01-27 17:48:01 -0400

    Implementar la gestión de maestro

--- server/src/main/java/com/github/angel/raa/modules/utils/DTO/AssignmentDTO.java
+++
@@ -1,7 +0,0 @@
-package com.github.angel.raa.modules.utils.DTO;
-
-public record AssignmentDTO(Long subjectId,
-
-                            Long teacherId,
-                            Long courseId) {
-}
