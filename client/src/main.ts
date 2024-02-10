635a824420d894c4c857339f585807a469fb193c
Angel Aguero <angelagueror23@gmail.com>
2024-02-10 10:04:18 -0400

    Integración de Mosha Vue Toastify para notificaciones emergentes

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,14 +1,16 @@
 import 'semantic-ui-css/semantic.min.css'
 import './assets/main.css'
 
+import 'mosha-vue-toastify/dist/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import moshaToast from 'mosha-vue-toastify'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(moshaToast)
 app.use(createPinia())
 app.use(router)
 
